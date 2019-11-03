b4f1a8cdd45bb27e5ccef347297aed0c4385f33e
ldesgoui <ldesgoui@gmail.com>
2019-11-03 05:28:03 +0100

    add help script

---
+++ contrib/6s_matches_and_unique_players_per_month.sql
@@ -0,0 +1,19 @@
+create temp table log_6s as
+select
+    id,
+    strftime("%Y-%m", date) as 'month'
+from log
+inner join player on log.id = player.log_id
+where title not like 'TF2Center Lobby #%'
+  and date between '2018-11-01' and '2019-11-01'
+group by id
+having sum(player.team = "Red") = 6
+    or sum(player.team = "Blue") = 6;
+
+select
+    month,
+    count(distinct id) as matches_played,
+    count(distinct player.steam_id) as unique_players
+from log_6s
+inner join player on log_6s.id = player.log_id
+group by month;
